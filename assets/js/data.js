aab368bd4ffc138157266a9b3df6967d75d21e0a
rafalq <kruszewskirafael@gmail.com>
2023-08-16 02:39:27 +0200

    refactor translation part

---
+++ assets/js/data.js
@@ -0,0 +1,238 @@
+const data = [
+  // certificates
+  {
+    pl: 'Dyplom programisty komputerowego',
+    en: 'Software Developer Diploma',
+  },
+
+  { pl: 'Podstawy Java', en: 'Java Foundations' },
+  {
+    pl: 'Kurs MySql',
+    en: 'MySql Bootcamp',
+  },
+  {
+    pl: 'Golang Praktyczny Przewodnik',
+    en: 'Golang The Practical Guide',
+  },
+  {
+    pl: 'Golang dla początkujących',
+    en: 'Golang for Beginners',
+  },
+  {
+    pl: 'Współczesny React i Redux',
+    en: 'Modern React and Redux',
+  },
+
+  {
+    pl: 'Certyfikaty',
+    en: 'Certificates',
+  },
+  // contact
+  {
+    pl: 'Wyślij Zapytanie',
+    en: 'Send Project Request',
+  },
+  {
+    pl: 'Współpracujmy razem!',
+    en: "Let's work together!",
+  },
+  {
+    pl: 'Zastanawiasz się nad jakimś projektem?',
+    en: 'Got a project in mind?',
+  },
+  { pl: 'Kontakt', en: 'Contact' },
+  // github
+  {
+    pl: 'Możesz także przejrzeć profile innych użytkowników ',
+    en: 'You can also search on other user profiles',
+  },
+  {
+    pl: "Obejrzyj moje projekty na Github'ie",
+    en: 'View My GitHub Projects',
+  },
+  // resume
+  { pl: 'Mój Github', en: 'My Github' },
+  { pl: 'Umiejętności', en: 'Skills' },
+  {
+    pl: 'Pracownik ogólny',
+    en: 'General Operative',
+  },
+  {
+    pl: 'Medtronic CardioVascular, Irlandia',
+    en: 'Medtronic CardioVascular, Ireland',
+  },
+  {
+    pl: 'Asystent dziennikarza radiowego',
+    en: 'Radio Journalist Assistant',
+  },
+  {
+    pl: 'Radio "Boss", Płock',
+    en: 'Radio "Boss", Poland',
+  },
+  {
+    pl: 'Doświadczenie',
+    en: 'Work History',
+  },
+  {
+    pl: 'Udemy, zdalnie',
+    en: 'Udemy, online',
+  },
+  {
+    pl: 'Frontend | Backend | Linux | Github',
+    en: 'Frontend | Backend | Linux | Github',
+  },
+  {
+    pl: 'Code Institute, Irlandia',
+    en: 'Code Institute, Ireland',
+  },
+  {
+    pl: 'Programista Full Stack',
+    en: 'Full Stack Web Developer',
+  },
+  {
+    pl: 'ecollege, Irlandia',
+    en: 'ecollege, Ireland',
+  },
+  {
+    pl: 'Java Associate Developer SE 8',
+    en: 'Java Associate Developer SE 8',
+  },
+  {
+    pl: 'Uniwersytet im. Adama Mickiewicza, Poznań',
+    en: 'Poznan University, Poland',
+  },
+  {
+    pl: 'Filozofia',
+    en: 'Philosophy',
+  },
+  {
+    pl: 'Wyższa Szkoła im. Pawła Włodkowica, Płock',
+    en: 'Pawel Wlodkowic College, Poland',
+  },
+  {
+    pl: 'Politologia',
+    en: 'Political Science',
+  },
+  {
+    pl: 'Edukacja',
+    en: 'Education',
+  },
+  {
+    pl: 'Życiorys',
+    en: 'Resume',
+  },
+  // about me
+  {
+    pl: 'Od sztuk walki do odważników kulowych.',
+    en: 'From martial arts to kettlebells.',
+  },
+  {
+    pl: 'Crossfit',
+    en: 'Crossfit',
+  },
+  {
+    pl: 'Gdybym urodził się w innych okolicznościach byłbym profesjonalnym muzykiem.',
+    en: 'If I had been born under different circumstances, I would have been professional musician.',
+  },
+  {
+    pl: 'Muzyka',
+    en: 'Music',
+  },
+  {
+    pl: '80% mojego czasu spędzam na kodowaniu lub uczeniu się nowych rzeczy o programowaniu.',
+    en: '80% of my time is spent coding or learning new things about programming.',
+  },
+  {
+    pl: 'Kodowanie',
+    en: 'Coding',
+  },
+  {
+    pl: 'Kiedy tylko mam trochę wolnego czasu, lubię czytać filozofię. Kiedyś lubiłem książki fantasy i sf. W sumie nadal lubię.',
+    en: 'Whenever I have some free time, I like to read philosophy. I used to like fantasy and sf books. All in all, I still do.',
+  },
+  {
+    pl: 'Literatura',
+    en: 'Literature',
+  },
+  {
+    pl: 'Zainteresowania',
+    en: 'Interests',
+  },
+  {
+    pl: 'Jestem zawsze efektywny w tym, co robię. Lubię pracować w grupie i dobrze się czuję w entuzjastycznie nastawionych zespołach. Jestem też samoukiem i nie mam nic przeciwko pracy solo.',
+    en: "I work effectively in getting things. I'm a keen team player who thrives in bright and enthusiastic teams and I'm also a self starter when working solo.",
+  },
+  {
+    pl: 'Dlaczego akurat mnie zatrudnić?',
+    en: 'Why hire me?',
+  },
+  {
+    pl: 'Kieruję się zarówno formą, jak i funkcjonalnością. Wiem, że estetyka efektywnego dizajn, połączonego z szybkością aplikacji, bezpieczeństwem i niezawodnością tworzą świetne wrażenia dla użytkownika.',
+    en: 'I follow form as well as function. I know that the aesthetics of effective design combined with application speed, security and reliability combine to create a great user experience.',
+  },
+  {
+    pl: 'Jak to robię?',
+    en: 'How do I do it?',
+  },
+  {
+    pl: 'Jestem Programistą Full Stack z różnorodnymi zainteresowaniami i wielkim entuzjazmem, który wnoszę do każdego projektu.',
+    en: "I'm a Full Stack Developer with diverse interests and great enthusiasm that I bring to every project.",
+  },
+  {
+    pl: 'Co robię?',
+    en: 'What do I do?',
+  },
+  {
+    pl: 'O Mnie',
+    en: 'About Me',
+  },
+  // header
+  {
+    pl: 'Programista Full Stack',
+    en: 'Full Stack Web Developer',
+  },
+  {
+    pl: 'Rafał Kruszewski',
+    en: 'Rafal Kruszewski',
+  },
+  // navbar
+  {
+    pl: 'Certyfikaty',
+    en: 'Certificates',
+  },
+  {
+    pl: 'O Mnie',
+    en: 'About Me',
+  },
+  {
+    // el: linkTextResume,
+    pl: 'Źyciorys',
+    en: 'Resume',
+  },
+  {
+    pl: 'Umiejętności',
+    en: 'Skills',
+  },
+  {
+    pl: 'Github',
+    en: 'Github',
+  },
+  {
+    pl: 'Kontakt',
+    en: 'Contact',
+  },
+  {
+    pl: 'Pobierz CV',
+    en: 'Download CV',
+  },
+  {
+    pl: 'Zmiana Tła',
+    en: 'Themify',
+  },
+  // lang switch
+  {
+    // el: switchTooltiptext,
+    pl: 'Translate into English',
+    en: 'Przetłumacz na polski',
+  },
+];
